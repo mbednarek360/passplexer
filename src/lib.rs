a2fd6c7da0fd8e03c13622a0ed61de74ad408e58
Michael Bednarek <mbednarek360@protonmail.com>
2020-02-13 15:04:07 -0500

    Added wasm boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 use gloo::{events::EventListener};
-//mod test;
+//mod pass;
 
 // run on load
 #[wasm_bindgen]
@@ -10,13 +10,12 @@ pub fn setup() {
     let window: web_sys::Window = web_sys::window().expect("");    
     let document: web_sys::Document = window.document().expect("");
     
-    // attach elements
-    let outp_header: web_sys::Element = document.get_element_by_id("outp_header").unwrap();
-    let main_button: web_sys::Element = document.get_element_by_id("main_button").unwrap();
+    // attach event listeners
+    EventListener::new(&document.get_element_by_id("main_button").unwrap(), "click", move |_event| {
 
-    // attach button listener
-    let button_listener: EventListener = EventListener::new(&main_button, "click", move |_event| {
-        outp_header.set_inner_html("World");
-    });
-    button_listener.forget();  
+    }).forget()
+
+
+
+
 }
