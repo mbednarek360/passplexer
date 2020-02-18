7ed40f6204dd2ab82911f92cb48c15dfcf294223
Michael Bednarek <mbednarek360@protonmail.com>
2020-02-18 12:52:01 -0500

    WIP Wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use gloo::{events::EventListener};
-//mod pass;
+//use web_sys::*;
+mod pass;
+
+// console log macro
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+#[macro_export]
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
 
 // run on load
 #[wasm_bindgen]
@@ -10,15 +23,13 @@ pub fn main() {
     let window: web_sys::Window = web_sys::window().expect("");    
     let document: web_sys::Document = window.document().expect("");
     
-    // attach event listeners
-    EventListener::new(&document.get_element_by_id("main_button").unwrap(), "click", move |_event| {
-        pass::pass_gen
-
-
-
-    }).forget()
-
-
-
-
+    // attach event listener
+    EventListener::new(&document.get_element_by_id("gen_button").unwrap(), "click", move |_event| {
+        console_log!("{}", pass::pass_gen(
+            document.get_element_by_id("username_input")
+                    .unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().value(),
+            document.get_element_by_id("master_input")
+                    .unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().value(),
+        64));
+    }).forget();
 }
--- src/pass.rs
+++ src/pass.rs
@@ -1,9 +1,29 @@
-pub fn pass_gen(service: String, username: String, master: String, salt: u32) -> String {
+use blake3;
+use zero85::ToZ85;
 
+// salt: map index for service list
+pub fn pass_gen(username: String, master: String, salt: u128) -> String {
 
+    // hash user and master
+    let user_hash: [u8; 32] = *blake3::hash(username.as_bytes()).as_bytes();
+    let master_hash: [u8; 32] = *blake3::hash(master.as_bytes()).as_bytes();
 
+    // interlace into single u8 array
+    let mut interlace: [u8; 64] = [0; 64];
+    for index in 0..user_hash.len() {
+        interlace[index * 2] = user_hash[index];
+        interlace[index * 2 + 1] = master_hash[index];
+    }
 
+    // generate key from salt
+    let mut hash_key: [u8; 32] = [0; 32];
+    let mut index = 0;
+    for byte in &salt.to_le_bytes() {
+        hash_key[index] = *byte;
+        hash_key[index + 16] = *byte;
+        index += 1;
+    }
 
-
-
+    // hash and encode into base 85
+    (*blake3::keyed_hash(&hash_key, &interlace).as_bytes()).to_z85().unwrap()
 }
