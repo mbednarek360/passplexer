df7239ea9265a8e117f907e497cf621036699cd9
Michael Bednarek <mbednarek360@protonmail.com>
2020-02-19 18:21:16 -0500

    First WASM working alpha

--- src/copy.js
+++ src/copy.js
@@ -1,3 +1,9 @@
-export function copy_clipboard(s) {
-    alert("bruh");
+export function copy_clipboard(text) {
+    var input = document.createElement('input');
+    input.setAttribute('value', text);
+    document.body.appendChild(input);
+    input.select();
+    var result = document.execCommand('copy');
+    document.body.removeChild(input)
+    return result;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use gloo::{events::EventListener};
-//use web_sys::*;
 mod pass;
 
 // console log macro
@@ -15,13 +14,10 @@ macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-// copy text
-fn copy_string(input: String) {
-
-
-
-
-    web_sys::ClipboardEvent::new("copy");
+// javascript interop
+#[wasm_bindgen(module = "/src/copy.js")]
+extern "C" {
+    fn copy_clipboard(text: String);
 }
 
 // run on load
@@ -32,16 +28,17 @@ pub fn main() {
     let window: web_sys::Window = web_sys::window().expect("");    
     let document: web_sys::Document = window.document().expect("");
     
-    // attach event listener
+    // attach event listener on button press
     EventListener::new(&document.get_element_by_id("gen_button").unwrap(), "click", move |_event| {
-        console_log!("{}", pass::pass_gen(
+
+        // send pass generated password to js
+        copy_clipboard(pass::pass_gen(
             document.get_element_by_id("username_input")
                     .unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().value(),
             document.get_element_by_id("master_input")
                     .unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().value(),
             document.get_element_by_id("salt_input")
                     .unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().value()
-                    .parse::<u64>().unwrap()
-        ));
+                    .parse::<u64>().unwrap()));
     }).forget();
 }
