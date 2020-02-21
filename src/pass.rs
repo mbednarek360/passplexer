3913b278e107280c0e2bdc0c9b723ae84b1322ed
Michael Bednarek <mbednarek360@protonmail.com>
2020-02-21 09:47:37 -0500

    Resized input hash

--- src/pass.rs
+++ src/pass.rs
@@ -1,15 +1,23 @@
 use blake3;
 use zero85::ToZ85;
 
+// slice to array
+fn from_slice(bytes: &[u8]) -> [u8; 16] {
+    let mut array = [0; 16];
+    let bytes = &bytes[..array.len()]; // panics if not enough data
+    array.copy_from_slice(bytes);
+    array
+}
+
 // salt: map index for service list
 pub fn pass_gen(username: String, master: String, salt: u64) -> String {
 
     // hash user and master
-    let user_hash: [u8; 32] = *blake3::hash(username.as_bytes()).as_bytes();
-    let master_hash: [u8; 32] = *blake3::hash(master.as_bytes()).as_bytes();
+    let user_hash: [u8; 16] = from_slice(&blake3::hash(username.as_bytes()).as_bytes()[0..16]);
+    let master_hash: [u8; 16] = from_slice(&blake3::hash(master.as_bytes()).as_bytes()[0..16]);
 
     // interlace into single u8 array
-    let mut interlace: [u8; 64] = [0; 64];
+    let mut interlace: [u8; 32] = [0; 32];
     for index in 0..user_hash.len() {
         interlace[index * 2] = user_hash[index];
         interlace[index * 2 + 1] = master_hash[index];
