29770954b5b4fa7a1549d820b9193c4b8a59cb0d
Michael Bednarek <mbednarek360@protonmail.com>
2020-02-28 21:43:18 -0500

    Added csv service map

--- src/id.js
+++
@@ -1,3 +0,0 @@
-export function get_source() {
-    return localStorage.getItem("id_source");
-}
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use gloo::{events::EventListener};
 mod pass;
-mod service;
 
 // javascript interop
 #[wasm_bindgen(module = "/src/copy.js")]
@@ -14,10 +13,12 @@ extern "C" {
 #[wasm_bindgen]
 pub fn main() {
 
+    // ease debugging
+    console_error_panic_hook::set_once();
+
     // setup dom
     let window: web_sys::Window = web_sys::window().expect("");    
     let document: web_sys::Document = window.document().expect("");
-    service::load_services();
 
     // attach event listener on button press
     EventListener::new(&document.get_element_by_id("gen_button").unwrap(), "click", move |_event| {
@@ -28,8 +29,8 @@ pub fn main() {
                     .unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().value(),
             document.get_element_by_id("master_input")
                     .unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().value(),
-            document.get_element_by_id("salt_input")
-                    .unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap().value()
+            document.get_element_by_id("service_input")
+                    .unwrap().dyn_into::<web_sys::HtmlSelectElement>().unwrap().value()
                     .parse::<u64>().unwrap()));
     }).forget();
 }
