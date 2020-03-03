b5e5067ac9f3ea61c5eaffe4140329b7e79deb81
Michael Bednarek <mbednarek360@protonmail.com>
2020-03-02 20:13:25 -0500

    Optimized build

--- build.sh
+++ build.sh
@@ -4,4 +4,8 @@ wasm-pack build --target web --release
 echo
 mv -v pkg src
 echo
-find src -name "*.js" -o -name ".wasm" | tar -cvjf passplexer_0.2.2.tar.bz2 -T -
+find src/pkg -type f ! -name '*.js' ! -name '*.wasm' -delete -print
+echo
+find src/pkg -type f ! -name '*.wasm' -exec minify {} -o {} \; -print
+echo
+find src -name "*.html" -o -name "*.js" -o -name "*.wasm" | tar -cvjf passplexer_0.2.2.tar.bz2 -T -
